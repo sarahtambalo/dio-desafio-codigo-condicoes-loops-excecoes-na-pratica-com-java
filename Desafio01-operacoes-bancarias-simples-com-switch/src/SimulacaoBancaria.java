e6f4ed171a64e8c90c1d4afad7d15aded085296b
Sarah Tambalo <sarahbianca90@gmail.com>
2024-06-28 21:37:12 -0300

    Desafio 02 / 05 - Verificação de Número de Conta Bancária

--- Desafio01-operacoes-bancarias-simples-com-switch/src/SimulacaoBancaria.java
+++ Desafio01-operacoes-bancarias-simples-com-switch/src/SimulacaoBancaria.java
@@ -13,14 +13,14 @@ public class SimulacaoBancaria {
 
             switch (opcao) {
                 case 1:
-                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    // TO DO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
                     double saldoAtual = scanner.nextDouble();
                     saldo += saldoAtual;
                     System.out.println("Saldo atual: " + saldo);
                     break;
                     
                 case 2:
-                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    // TO DO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
                     double valorSaque = scanner.nextDouble();
                     if(valorSaque > saldo){
                       System.out.println("Saldo insuficiente.");
@@ -31,7 +31,7 @@ public class SimulacaoBancaria {
                     break;
                     
                 case 3:
-                    // TODO: Exibir o saldo atual da conta.
+                    // TO DO: Exibir o saldo atual da conta.
                     System.out.println("Saldo atual: " + saldo);
                     break;
                     
---
+++ Desafio02-verifica-numero-conta-bancaria/src/VerificadorNumeroConta.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class VerificadorNumeroConta {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String numeroConta = scanner.nextLine();
+
+            // TO DO: Chamar o método que veritica se o número da conta é valido
+            verificarNumeroConta(numeroConta);
+            
+            // Caso nenhuma exceção seja lançada, imprime a mensagem de sucesso.
+            System.out.println("Numero de conta valido.");
+            
+        } catch (IllegalArgumentException e) {
+            // TO DO: Informar que o número de conta é inválido e exibir a mensagem de erro
+            System.out.println("Erro: " + e.getMessage());
+        } finally {
+            // Fechar o scanner para evitar vazamentos de recursos
+            scanner.close();
+        }
+    }
+
+    private static void verificarNumeroConta(String numeroConta) {
+        int contadorDeNumero = numeroConta.length();
+        int numeroDigitado = 0;
+        for (int i = 0; i < contadorDeNumero; i++){
+          numeroDigitado++;
+        }
+        
+            // TO DO: Lançar uma IllegalArgumentException com a seguinte mensagem:
+            // "Numero de conta invalido. Digite exatamente 8 digitos."
+        if (numeroConta.length() != 8) {
+            throw new IllegalArgumentException("Numero de conta invalido. Digite exatamente 8 digitos.");
+        }
+    }
+}
